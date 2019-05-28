0375402cf8c58a266a21af498b72aaf9d47d8f62
Ariel Alonso <alonso.ariel90@gmail.com>
2019-05-28 13:25:00 -0300

    Teste Gabriel

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 module.exports = app => {
 
     app.get("/", (req, res) => {
-        res.json({status: "Node Master API"})
+        res.json({status: "Node Master API - Teste gabriel"})
     });
     
 };
